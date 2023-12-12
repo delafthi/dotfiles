69485987d5c10e82590bcc72b336aa48dcd83846
Thierry Delafontaine <deaa@zhaw.ch>
2023-12-12 15:56:15 +0100

    feat: update configuration
    - set `escapeTime` to 0
    - increase the `historyLimit` to 10000
    - use the default for `reverseSplit`
    - set `terminal` to 'screen-256color'

--- home-manager/programs/tmux.nix
+++ home-manager/programs/tmux.nix
@@ -3,9 +3,11 @@
     enable = true;
     baseIndex = 1;
     clock24 = true;
+    escapeTime = 0;
+    historyLimit = 10000;
     keyMode = "vi";
     mouse = true;
     newSession = true;
-    reverseSplit = true;
+    terminal = "screen-256color";
   };
 }
