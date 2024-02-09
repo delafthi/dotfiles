95349090180530564ed621bf8099e6c447d151ea
Thierry Delafontaine <thierry@delafontaine.xyz>
2024-02-09 08:29:28 +0100

    fix: change volume and options ordering

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -15,41 +15,42 @@
     {
       device = "/dev/disk/by-uuid/21301508-a3f6-477e-9c7b-6e36c8fa3b0f";
       fsType = "btrfs";
-      options = [ "compress=zstd" "subvol=root" ];
-    };
-
-  fileSystems."/home" =
-    {
-      device = "/dev/disk/by-uuid/21301508-a3f6-477e-9c7b-6e36c8fa3b0f";
-      fsType = "btrfs";
-      options = [ "compress=zstd" "subvol=home" ];
-    };
-
-  fileSystems."/nix" =
-    {
-      device = "/dev/disk/by-uuid/21301508-a3f6-477e-9c7b-6e36c8fa3b0f";
-      fsType = "btrfs";
-      options = [ "compress=zstd" "noatime" "subvol=nix" ];
-    };
-
-  fileSystems."/var" =
-    {
-      device = "/dev/disk/by-uuid/21301508-a3f6-477e-9c7b-6e36c8fa3b0f";
-      fsType = "btrfs";
-      options = [ "compress=zstd" "noatime" "subvol=var" ];
-    };
-
-  fileSystems."/swap" =
-    {
-      device = "/dev/disk/by-uuid/21301508-a3f6-477e-9c7b-6e36c8fa3b0f";
-      fsType = "btrfs";
-      options = [ "noatime" "subvol=swap" ];
+      options = [ "subvol=root" "compress=zstd" ];
     };
 
   fileSystems."/boot" =
     {
       device = "/dev/disk/by-uuid/B0A0-692B";
       fsType = "vfat";
+    };
+
+
+  fileSystems."/home" =
+    {
+      device = "/dev/disk/by-uuid/21301508-a3f6-477e-9c7b-6e36c8fa3b0f";
+      fsType = "btrfs";
+      options = [ "subvol=home" "compress=zstd" ];
+    };
+
+  fileSystems."/nix" =
+    {
+      device = "/dev/disk/by-uuid/21301508-a3f6-477e-9c7b-6e36c8fa3b0f";
+      fsType = "btrfs";
+      options = [ "subvol=nix" "compress=zstd" "noatime" ];
+    };
+
+  fileSystems."/swap" =
+    {
+      device = "/dev/disk/by-uuid/21301508-a3f6-477e-9c7b-6e36c8fa3b0f";
+      fsType = "btrfs";
+      options = [ "subvol=swap" "noatime" ];
+    };
+
+  fileSystems."/var" =
+    {
+      device = "/dev/disk/by-uuid/21301508-a3f6-477e-9c7b-6e36c8fa3b0f";
+      fsType = "btrfs";
+      options = [ "subvol=var" "compress=zstd" "noatime" ];
     };
 
   swapDevices = [{ device = "/swap/swapfile"; }];
