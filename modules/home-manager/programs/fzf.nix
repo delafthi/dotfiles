3cb17e58a6e8f7691a2446e5e6568d22daff2655
Thierry Delafontaine <thierry@delafontaine.xyz>
2024-11-02 20:26:33 +0100

    fix: color overrides not necessary to look good

--- modules/home-manager/programs/fzf.nix
+++ modules/home-manager/programs/fzf.nix
@@ -2,21 +2,6 @@
   programs.fzf = {
     enable = true;
     changeDirWidgetCommand = "fd --type d";
-    colors = {
-      fg = "#abb2bf";
-      "fg+" = "#c8cdd5";
-      bg = "#282c34";
-      "bg+" = "#22262d";
-      hl = "#e5c07b";
-      "hl+" = "#e5c07b";
-      border = "#3e4451";
-      header = "#e06c75";
-      info = "#98c379";
-      marker = "#d19a66";
-      pointer = "#be5046";
-      prompt = "#c678dd";
-      spinner = "#c678dd";
-    };
     defaultCommand = "fd --type f";
     fileWidgetCommand = "fd --type f";
     tmux = {
