403e8569ad7c78793668bd2defb416889e410a69
Thierry Delafontaine <thierry@delafontaine.xyz>
2024-11-04 22:07:03 +0100

    fix: fix tmux running bash-3.2 instead of user shell

--- modules/home-manager/programs/tmux.nix
+++ modules/home-manager/programs/tmux.nix
@@ -31,12 +31,13 @@
       set -g window-status-current-format "#[fg=brightblack,nobold,bg=default][#[fg=brightblack,nobold,bg=default]#I #[fg=magenta,nobold,bg=default]#F #[fg=blue,blue,bold,bg=default]#W#[fg=brightblack,nobold,bg=default]]"
       set -g window-status-format "#[fg=brightblack,nobold,bg=default][#[fg=brightblack,bg=default]#I #F #[fg=white,bg=default]#W#[fg=brightblack,nobold,bg=default]]"
       set-option -g detach-on-destroy off
+      set-option -gu default-command
+      set-option -g default-shell "$SHELL"
     '';
     historyLimit = 50000;
     keyMode = "vi";
     mouse = true;
     secureSocket = true;
-    shell = "$SHELL";
     terminal = "tmux-256color";
   };
   xdg.configFile."tmux/ansi2tmux.pl" = {
