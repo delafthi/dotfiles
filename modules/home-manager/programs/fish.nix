33d191ae6e8232fbbda2318fd6346482634263d8
Thierry Delafontaine <thierry@delafontaine.xyz>
2024-11-05 14:19:24 +0100

    feat: add keybindings for ctrl-(jknp)

--- modules/home-manager/programs/fish.nix
+++ modules/home-manager/programs/fish.nix
@@ -12,7 +12,12 @@
     interactiveShellInit = ''
       set fish_greeting
       set -g fish_key_bindings fish_vi_key_bindings
+      bind -M insert \ck up-or-search
+      bind -M insert \cj down-or-search
       bind -M insert \cl accept-autosuggestion
+      bind -M insert \cp up-or-search
+      bind -M insert \cn complete
+      bind -M insert \cs pager-toggle-search
     '';
     plugins = [
       {
