c490bdd6e3846ea9a782f4ddf80dd68f38bb7f8d
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-01-10 18:16:32 +0100

    fix: sort apps alphabetically

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -23,8 +23,6 @@
     masApps = {
       "AdGuard for Safari" = 1440147259;
       "Apple Delevoper" = 640199958;
-      Testflight = 899247664;
-      XCode = 497799835;
       Bear = 1091189122;
       Habits = 1514915737;
       Hush = 1544743900;
@@ -35,9 +33,11 @@
       Photomator = 1444636541;
       "Proton Pass" = 6443490629;
       Shazam = 897118787;
+      Testflight = 899247664;
       Things = 904280696;
       "Toggl Track" = 1291898086;
       WhatsApp = 310633997;
+      XCode = 497799835;
     };
     onActivation = {
       autoUpdate = true;
