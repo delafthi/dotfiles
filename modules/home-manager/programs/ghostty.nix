b345d56023592a679bf42568b139cdc96a63e05a
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-02-02 17:33:23 +0100

    feat: configure ghostty through the programs interface

--- modules/home-manager/programs/ghostty.nix
+++ modules/home-manager/programs/ghostty.nix
@@ -2,11 +2,17 @@
 
 
   home.packages = [ pkgs.nerd-fonts.iosevka-term-slab ];
-  xdg.configFile."ghostty/config".text = ''
-    font-family = "IosevkaTermSlab Nerd Font Mono"
-    font-size = 14
-    theme = tokyonight_night
-    window-padding-x = 8
-  '';
-
+  programs.ghostty = {
+    enable = true;
+    package = null;
+    enableBashIntegration = true;
+    enableFishIntegration = true;
+    enableZshIntegration = true;
+    settings = {
+      font-family = "IosevkaTermSlab Nerd Font Mono";
+      font-size = 14;
+      theme = "tokyonight_night";
+      window-padding-x = 8;
+    };
+  };
 }
