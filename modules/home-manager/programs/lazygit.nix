a88a03d657b1774172716ee7fe24997e6d5641e6
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-02-05 00:38:49 +0100

    fix: style

--- modules/home-manager/programs/lazygit.nix
+++ modules/home-manager/programs/lazygit.nix
@@ -7,10 +7,10 @@
   programs.lazygit = {
     enable = true;
     settings = {
-      gui.nerdFontsVersion = "3";
       git.paging = {
         externalDiffCommand = "difft --color=always --display=side-by-side-show-both";
       };
+      gui.nerdFontsVersion = "3";
     };
   };
 }
