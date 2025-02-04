1ecfd28bc53937ae9cc1f10ced448cb7b819915a
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-02-04 13:45:22 +0100

    feat: add a binding to open yazi in a popup
    The 'f' key is used for the file browser. Consequently, the key to open
    the tmux-sessionizer has been moved to 's'

--- modules/home-manager/programs/tmux/default.nix
+++ modules/home-manager/programs/tmux/default.nix
@@ -20,9 +20,10 @@
       bind -N "Source the tmux config file" R run-shell "\
         tmux source-file ~/.config/tmux/tmux.conf > /dev/null; \
         tmux display-message 'Sourced ~/.config/tmux/tmux.conf'"
+      bind -N "Open file browser" f popup -h 90% -w 90% "${pkgs.yazi}/bin/yazi"
       bind -N "Open lazygit" g popup -h 90% -w 90% -E "${pkgs.lazygit}/bin/lazygit"
+      bind -N "Open tmux-sessioinizer" s popup -h 60% -w 60% -E "~/.local/bin/tmux-sessionizer"
       bind -N "Open scratch terminal" t popup -h 90% -w 90% ""
-      bind -N "Open tmux-sessioinizer" f popup -h 60% -w 60% -E "~/.local/bin/tmux-sessionizer"
 
 
       # statusbar
