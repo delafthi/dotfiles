5434e6111f7e57479ce4727fc74fc0d10ab1df46
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-02-04 20:55:19 +0100

    fix: move files defined inline into dedicated files

---
+++ modules/home-manager/programs/gtkwave/default.nix
@@ -0,0 +1,7 @@
+{ pkgs ? import <nixpkgs> { } }: {
+  home = {
+    file.".gtkwaverc".source = ./.gtkwaverc;
+    file."gtkwave.tcl".source = ./gtkwave.tcl;
+    packages = [ pkgs.gtkwave ];
+  };
+}
