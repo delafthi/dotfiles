fdfd0ff531faf3af4336e1ed85ae93c4a4e6841c
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-02-05 14:47:16 +0100

    feat: use bat as manpager

--- modules/home-manager/programs/bat.nix
+++ modules/home-manager/programs/bat.nix
@@ -1,4 +1,9 @@
 { tokyonight }: {
+  home = {
+    sessionVariables = {
+      MANPAGER = "sh -c 'sed -u -e \\\"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\\\" | bat -p -lman'";
+    };
+  };
   programs.bat = {
     enable = true;
     config = {
