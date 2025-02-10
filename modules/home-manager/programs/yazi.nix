3f31cfd5ef8a1db6f42c9b88b8136d0ddbd0e920
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-02-10 15:55:36 +0100

    feat: add yazi manager options to change ratio, show hidden files and sort directories first

--- modules/home-manager/programs/yazi.nix
+++ modules/home-manager/programs/yazi.nix
@@ -4,8 +4,11 @@
   programs.yazi = {
     enable = true;
     settings = {
-      manager.showhidden = true;
-      sort_dir_fist = true;
+      manager = {
+        ratio = [ 2 4 3 ];
+        sort_dir_fist = true;
+        showhidden = true;
+      };
     };
     shellWrapperName = "y";
     theme = lib.importTOML "${tokyonight}/extras/yazi/tokyonight_night.toml";
