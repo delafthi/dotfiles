72da1d82c3e709bbedc8a9b6637cd0082f7e64b5
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-02-26 18:34:57 +0100

    fix: fix invoking alt+<key> characters in ghostty

--- modules/home-manager/programs/ghostty.nix
+++ modules/home-manager/programs/ghostty.nix
@@ -11,6 +11,7 @@
     settings = {
       font-family = "IosevkaTermSlab Nerd Font Mono";
       font-size = 14;
+      macos-option-as-alt = false;
       theme = "tokyonight_night";
       window-padding-x = 8;
     };
