4442191bcfad18184f81d137436674523d205ee4
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-03-28 16:39:02 +0100

    feat: use bat as default pager

--- modules/home-manager/programs/bat.nix
+++ modules/home-manager/programs/bat.nix
@@ -2,6 +2,7 @@
   home = {
     sessionVariables = {
       MANPAGER = "sh -c 'sed -u -e \\\"s/\\x1B\[[0-9;]*m//g; s/.\\x08//g\\\" | bat -p -lman'";
+      PAGER = "bat -p";
     };
   };
   programs.bat = {
