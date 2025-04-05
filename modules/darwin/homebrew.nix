6389354a686885625bdf61c280e0e404f68e52f2
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-04-05 17:48:28 +0200

    fix: temporarily disable install mas apps with homebrew
    Currently, mas apps are always upgraded/installed which takes a very
    long time.

--- modules/darwin/homebrew.nix
+++ modules/darwin/homebrew.nix
@@ -28,32 +28,32 @@
         "rocket"
         "zen-browser"
       ];
-    masApps = {
-      AnyConnect = 1135064690;
-      Developer = 640199958;
-      Bear = 1091189122;
-      Deepstash = 1445023295;
-      Delta = 1048524688;
-      Habits = 1514915737;
-      "Hand Mirror" = 1502839586;
-      Keynote = 409183694;
-      MeteoSwiss = 589772015;
-      Numbers = 409203825;
-      "Organic Maps" = 1567437057;
-      Pages = 409201541;
-      PDFgear = 6469021132;
-      Photomator = 1444636541;
-      "Proton Pass" = 6443490629;
-      "Proton Wallet" = 479609548;
-      "SAC-CAS" = 1592646841;
-      Shazam = 897118787;
-      stoic = 1312926037;
-      Swisstopo = 1505986543;
-      Testflight = 899247664;
-      Things = 904280696;
-      WhatsApp = 310633997;
-      XCode = 497799835;
-    };
+    # masApps = {
+    #   AnyConnect = 1135064690;
+    #   Developer = 640199958;
+    #   Bear = 1091189122;
+    #   Deepstash = 1445023295;
+    #   Delta = 1048524688;
+    #   Habits = 1514915737;
+    #   "Hand Mirror" = 1502839586;
+    #   Keynote = 409183694;
+    #   MeteoSwiss = 589772015;
+    #   Numbers = 409203825;
+    #   "Organic Maps" = 1567437057;
+    #   Pages = 409201541;
+    #   PDFgear = 6469021132;
+    #   Photomator = 1444636541;
+    #   "Proton Pass" = 6443490629;
+    #   "Proton Wallet" = 479609548;
+    #   "SAC-CAS" = 1592646841;
+    #   Shazam = 897118787;
+    #   stoic = 1312926037;
+    #   Swisstopo = 1505986543;
+    #   Testflight = 899247664;
+    #   Things = 904280696;
+    #   WhatsApp = 310633997;
+    #   XCode = 497799835;
+    # };
     onActivation = {
       autoUpdate = true;
       cleanup = "zap";
