90aa76e4f786d5eab45d2c39d6581a6aaf37654a
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-04-09 10:06:16 +0200

    feat: update the global gitignore file
    Used https://github.com/github/gitignore/tree/main/Global as source.

--- modules/home-manager/programs/git.nix
+++ modules/home-manager/programs/git.nix
@@ -70,11 +70,119 @@
       tag.sort = "version:refname";
     };
     ignores = [
-      ".cache/"
-      ".direnv/"
+      # github/gitignores/Global/Archives.gitignore
+      # It's better to unpack these files and commit the raw source because
+      # git has its own built in compression methods.
+      "*.7z"
+      "*.jar"
+      "*.rar"
+      "*.zip"
+      "*.gz"
+      "*.gzip"
+      "*.tgz"
+      "*.bzip"
+      "*.bzip2"
+      "*.bz2"
+      "*.xz"
+      "*.lzma"
+      "*.cab"
+      "*.xar"
+      "*.zst"
+      "*.tzst"
+      # Packing-only formats
+      "*.iso"
+      "*.tar"
+      # Package management formats
+      "*.dmg"
+      "*.xpi"
+      "*.gem"
+      "*.egg"
+      "*.deb"
+      "*.rpm"
+      "*.msi"
+      "*.msm"
+      "*.msp"
+      "*.txz"
+
+      # github/gitignores/Global/Backup.gitignore
+      "*.bak"
+      "*.gho"
+      "*.ori"
+      "*.orig"
+      "*.tmp"
+
+      # github/gitignores/Global/GPG.gitignore
+      "secring.*"
+
+      # github/gitignores/Global/Linux.gitignore
+      "*~"
+      # temporary files which can be created if a process still has a handle open of a deleted file
+      ".fuse_hidden*"
+      # Metadata left by Dolphin file manager, which comes with KDE Plasma
+      ".directory"
+      # Linux trash folder which might appear on any partition or disk
+      ".Trash-*"
+      # .nfs files are created when an open file is removed but is still being accessed
+      ".nfs*"
+      # Log files created by default by the nohup command
+      "nohup.out"
+
+      # github/gitignores/Global/macOS.gitignore
+      # General
       ".DS_Store"
-      ".gdb_history"
-      ".python-version"
+      ".AppleDouble"
+      ".LSOverride"
+      # Thumbnails
+      "._*"
+      # Files that might appear in the root of a volume
+      ".DocumentRevisions-V100"
+      ".fseventsd"
+      ".Spotlight-V100"
+      ".TemporaryItems"
+      ".Trashes"
+      ".VolumeIcon.icns"
+      ".com.apple.timemachine.donotpresent"
+      # Directories potentially created on remote AFP share
+      ".AppleDB"
+      ".AppleDesktop"
+      "Network Trash Folder"
+      "Temporary Items"
+      ".apdisk"
+
+      # github/gitignores/Global/SVN.gitignore
+      ".svn/"
+
+      # github/gitignores/Global/Syncthing.gitignore
+      # Syncthing caches
+      ".stversions"
+
+      # github/gitignores/Global/Tags.gitignore
+      # Ignore tags created by etags, ctags, gtags (GNU global) and cscope
+      "TAGS"
+      ".TAGS"
+      "!TAGS/"
+      "tags"
+      ".tags"
+      "!tags/"
+      "gtags.files"
+      "GTAGS"
+      "GRTAGS"
+      "GPATH"
+      "GSYMS"
+      "cscope.files"
+      "cscope.out"
+      "cscope.in.out"
+      "cscope.po.out"
+
+      # github/gitignores/Global/VirtualEnv.gitignore
+      ".venv"
+
+      # Other common directories I don't want to be included
+      ".cache"
+      ".direnv"
+      ".env"
+      ".git"
+      ".jj"
       "compile_commands.json"
     ];
     lfs = {
