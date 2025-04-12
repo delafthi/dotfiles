ba606a8b0621aa168e5a1105b12516f8c1b45e44
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-04-12 16:00:26 +0200

    feat: use caffeine instead of keepawake to disable sleeping on gnome

--- modules/home-manager/linux/programs/gnome-shell.nix
+++ modules/home-manager/linux/programs/gnome-shell.nix
@@ -3,9 +3,9 @@
     enable = true;
     extensions = with pkgs.gnomeExtensions; [
       {package = blur-my-shell;}
+      {package = caffeine;}
       {package = dash-to-dock;}
       {package = gnome-40-ui-improvements;}
-      {package = keep-awake;}
       {package = move-clock;}
       {package = notification-banner-position;}
       {package = search-light;}
