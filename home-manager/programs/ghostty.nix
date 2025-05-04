54b8c7dccc3930abb95a39878969bcc5e108947e
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-05-04 19:33:50 +0200

    feat(ghostty): set cursor shape to a bar

--- home-manager/programs/ghostty.nix
+++ home-manager/programs/ghostty.nix
@@ -6,6 +6,7 @@
     # The ghostty package is currently broken on darwin
     package = if pkgs.hostPlatform.isDarwin then null else pkgs.ghostty;
     settings = {
+      cursor-style = "bar";
       font-family = "IosevkaTermSlab Nerd Font Mono";
       font-size = 14;
       macos-option-as-alt = false;
