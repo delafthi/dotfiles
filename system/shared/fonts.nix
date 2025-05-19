a096b0402627438e6d4d3ca1ee5296e87aa305cf
Thierry Delafontaine <thierry@delafontaine.xyz>
2025-05-19 20:48:00 +0200

    feat(system): install iosevka fonts
    install the iosevka font and variants aile and etoile

--- system/shared/fonts.nix
+++ system/shared/fonts.nix
@@ -2,7 +2,9 @@
 {
   fonts = {
     packages = with pkgs; [
-      iosevka
+      iosevka-bin
+      (iosevka-bin.override { variant = "Aile"; })
+      (iosevka-bin.override { variant = "Etoile"; })
       nerd-fonts.fira-code
       nerd-fonts.iosevka-term-slab
       nerd-fonts.symbols-only
