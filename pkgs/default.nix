42e5b4d7e845547e1b04a47f613a19e398873107
Thierry Delafontaine <delafthi@pm.me>
2025-06-12 20:14:53 +0200

    feat(ai): add various mcp servers
    - added package for codex-rs, since codex-cli doesn't support mcp

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,5 @@
 pkgs: {
+  codex-rs = pkgs.callPackage ./codex-rs.nix { };
   tmux-gh-dash = pkgs.callPackage ./tmux-gh-dash { };
   tmux-scratch-terminal = pkgs.callPackage ./tmux-scratch-terminal { };
   tmux-sessionizer = pkgs.callPackage ./tmux-sessionizer { };
