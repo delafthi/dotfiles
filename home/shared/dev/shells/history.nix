e33213195a8f9c56faa707160a6bf2013e92739d
Thierry Delafontaine <delafthi@pm.me>
2025-06-13 16:57:21 +0200

    feat(shell): improve atuin configs
    - Set dialect to  UK dialect.
    - Added a filter to not store exporting API keys
    - Added filter to not store the `exit` command

--- home/shared/dev/shells/history.nix
+++ home/shared/dev/shells/history.nix
@@ -3,11 +3,14 @@
     enable = true;
     daemon.enable = true;
     settings = {
+      dialect = "uk";
       filter_mode_shell_up_key_binding = "directory";
-      invert = true;
       history_filter = [
+        "_API_KEY="
         "^cd"
+        "^exit$"
       ];
+      invert = true;
     };
   };
 }
