8f967df5092648c8b98d74d4d5a7f9200baf002f
Thierry Delafontaine <delafthi@pm.me>
2025-06-18 11:32:24 +0200

    fix(editorconfig): update editorconfig file to the my default

--- home/shared/dev/espanso.nix
+++ home/shared/dev/espanso.nix
@@ -39,10 +39,10 @@
               [*]
               charset = utf-8
               end_of_line = lf
-              trim_trailing_whitespace = true
               indent_style = space
               indent_size = 2
               insert_final_newline = true
+              trim_trailing_whitespace = true
 
               [{Justfile,Makefile}]
               indent_style = tab
