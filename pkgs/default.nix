17bffe6cf1a5b3b546a9c4fa7288fca3ec55c66e
Thierry Delafontaine <delafthi@pm.me>
2025-06-29 10:24:32 +0200

    feat(opencode): 0.1.126 -> 0.1.162

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,5 +1,5 @@
 pkgs: {
-  opencode = pkgs.callPackage ./opencode.nix { };
+  opencode = pkgs.callPackage ./opencode { };
   tmux-gh-dash = pkgs.callPackage ./tmux-gh-dash { };
   tmux-scratch-terminal = pkgs.callPackage ./tmux-scratch-terminal { };
   tmux-sessionizer = pkgs.callPackage ./tmux-sessionizer { };
