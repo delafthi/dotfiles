aefcc601cb6944e7817d47c68c0277986016a361
Thierry Delafontaine <delafthi@pm.me>
2025-07-04 13:55:59 +0200

    refactor: move treefmt configuration to a separate file
    - Extracted the treefmt configuration from flake.nix and created a new
    file nix/treefmt.nix to hold the configuration

--- flake.nix
+++ flake.nix
@@ -144,27 +144,7 @@
                 ];
               };
             };
-            treefmt = {
-              projectRootFile = "flake.nix";
-              programs = {
-                actionlint.enable = true;
-                deadnix.enable = true;
-                fish_indent.enable = true;
-                keep-sorted.enable = true;
-                nixfmt.enable = true;
-                prettier.enable = true;
-                shellcheck.enable = true;
-                shfmt.enable = true;
-                statix.enable = true;
-              };
-              settings.formatter = {
-                editorconfig-checker = {
-                  command = pkgs.editorconfig-checker;
-                  includes = [ "*" ];
-                  priority = 9; # last
-                };
-              };
-            };
+            treefmt = import ./nix/treefmt.nix;
           };
       }
     );
