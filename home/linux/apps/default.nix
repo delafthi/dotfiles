7e8b8231854dd07ec9c23048bfecd8c2767fc7e0
Thierry Delafontaine <delafthi@pm.me>
2025-07-05 23:36:24 +0200

    feat(home): migrate homebrew packages to nixpkgs
    - install element with nixpkgs
    - install shottr with nixpkgs

--- home/linux/apps/default.nix
+++ home/linux/apps/default.nix
@@ -5,7 +5,6 @@
   ];
   home.packages = with pkgs; [
     ascii-draw
-    element-desktop
     krita
     mpv
     obsidian
--- home/shared/apps/default.nix
+++ home/shared/apps/default.nix
@@ -5,6 +5,7 @@
   ];
   home.packages = with pkgs; [
     blender
+    element-desktop
     vesktop
     zotero
   ];
