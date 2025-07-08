0573faaf24f76389791105d4716066f2d5b60042
Thierry Delafontaine <delafthi@pm.me>
2025-07-08 22:36:01 +0200

    feat(nix): enhance Nix configuration with additional options and garbage collection settings
    - Added `extraOptions` for Nix with parameters for connection timeout and output retention.
    - Configured garbage collection with a specific interval for automatic execution.
    - Improved optimization settings to include a dynamic interval based on garbage collection timing.

--- system/shared/nix.nix
+++ system/shared/nix.nix
@@ -1,8 +1,29 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
 {
   nix = {
-    gc.automatic = true;
-    optimise.automatic = true;
+    extraOptions = ''
+      connect-timeout = 10
+      keep-derivations = true
+      keep-going = true
+      keep-outputs = true
+    '';
+    gc = {
+      automatic = true;
+      interval = [
+        {
+          Hour = 3;
+          Minute = 15;
+          Weekday = 1;
+        }
+      ];
+    };
+    optimise = {
+      automatic = true;
+      interval = lib.lists.forEach config.nix.gc.interval (e: {
+        inherit (e) Minute Weekday;
+        Hour = e.Hour + 1;
+      });
+    };
     settings = {
       experimental-features = [
         "nix-command"
