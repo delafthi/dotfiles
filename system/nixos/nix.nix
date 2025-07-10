bfe0ce0c7e8d1179a6dd200e29c55d6cd049d1b3
Thierry Delafontaine <delafthi@pm.me>
2025-07-10 09:31:22 +0200

    feat(nix): make system more responsive during build
    Added configuration that builds run with lower priority to keep the
    system responsive.

--- system/nixos/nix.nix
+++ system/nixos/nix.nix
@@ -1,5 +1,8 @@
 {
   nix = {
+    daemonCPUSchedPolicy = "batch";
+    daemonIOSchedClass = "idle";
+    daemonIOSchedPriority = 7;
     gc.dates = [ "03:15" ];
     optimise.dates = [ "04:15" ];
   };
