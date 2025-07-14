0ef376c75cbf3a27d91efa1447d71d5959191b6f
Thierry Delafontaine <delafthi@pm.me>
2025-07-14 13:38:36 +0200

    feat: add qmk

--- home/shared/apps/default.nix
+++ home/shared/apps/default.nix
@@ -3,6 +3,7 @@
   home.packages = with pkgs; [
     blender
     element-desktop
+    qmk
     signal-desktop-bin
     vesktop
     zotero
--- system/nixos/hardware/default.nix
+++ system/nixos/hardware/default.nix
@@ -4,6 +4,7 @@
     ./ergodox-keyboard.nix
     ./logitech-mouse.nix
     ./printing.nix
+    ./qmk.nix
     ./yubikey.nix
   ];
 }
