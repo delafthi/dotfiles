28602a168452bfe81bac185b404b74b1bdc229c7
Thierry Delafontaine <delafthi@pm.me>
2025-09-22 13:30:41 +0200

    refactor(code-review): use jj diff instead of git diff in review script

--- pkgs/code-review/review.fish
+++ pkgs/code-review/review.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 
 if test (count $argv) -eq 0
-    git diff --context 5 | mods --format --quiet --raw --role review-code | glow
+    jj diff --git --context 5 --no-pager | mods --format --quiet --raw --role review-code | glow
 else
     mods --format --quiet --raw --role review-code $argv | glow
 end
