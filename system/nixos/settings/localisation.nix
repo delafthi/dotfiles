898add87a1fcb9d3cb6e7b9b96762786dfcd660a
Thierry Delafontaine <delafthi@pm.me>
2025-09-26 15:19:37 +0200

    fix(localization): set LANG to en_US

--- system/nixos/settings/localisation.nix
+++ system/nixos/settings/localisation.nix
@@ -1,7 +1,10 @@
 {
   i18n = {
     defaultLocale = "de_CH.UTF-8";
-    extraLocaleSettings.LC_MESSAGES = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LANG = "en_US.UTF-8";
+      LC_MESSAGES = "en_US.UTF-8";
+    };
   };
   time.timeZone = "Europe/Zurich";
 }
