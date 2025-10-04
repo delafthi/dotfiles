d5a6225c8f1a5df461fcbe2ecc0b6fb7bcd436b8
Thierry Delafontaine <delafthi@pm.me>
2025-10-04 09:08:49 +0200

    feat(dev): scrolling in ghostty should now be smoother

--- home/shared/dev/ghostty.nix
+++ home/shared/dev/ghostty.nix
@@ -13,6 +13,7 @@
         "cmd+k=text:\\x00k" # opens tmux-sessionizer
       ];
       macos-option-as-alt = "left";
+      mouse-scroll-multiplier = "1";
       window-inherit-working-directory = false;
       window-padding-x = 8;
     };
