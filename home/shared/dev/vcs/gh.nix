13fe139ac018b3289a2991fb9c54acad8ff37081
Thierry Delafontaine <delafthi@pm.me>
2025-10-11 14:02:22 +0200

    fix(vcs): remove `gh` wrapper for `jj` only repositonries

--- home/shared/dev/vcs/gh.nix
+++ home/shared/dev/vcs/gh.nix
@@ -1,20 +1,5 @@
 { pkgs, ... }:
 {
-  home = {
-    file.".local/bin/gh-wrapped" = {
-      text = ''
-        JJ_ROOT=$(jj root --ignore-working-copy); \
-        if [ $? -eq 0 ]; then \
-          GIT_DIR=$JJ_ROOT/.jj/repo/store/git gh $@; \
-        else \
-          gh $@; \
-        fi
-      '';
-      executable = true;
-    };
-    packages = with pkgs; [ bat ];
-    shellAliases.gh = "gh-wrapped";
-  };
   programs = {
     gh = {
       enable = true;
