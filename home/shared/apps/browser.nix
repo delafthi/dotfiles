6a56372440744967de1b23f047087b087126c9b5
Thierry Delafontaine <delafthi@pm.me>
2025-11-04 16:17:18 +0100

    feat(zen-browser): add enterprise policies for privacy and security

--- home/shared/apps/browser.nix
+++ home/shared/apps/browser.nix
@@ -3,5 +3,23 @@
   programs.zen-browser = {
     enable = true;
     nativeMessagingHosts = lib.optionals pkgs.stdenv.hostPlatform.isLinux [ pkgs.firefoxpwa ];
+    policies = {
+      AutofillAddressEnabled = true;
+      AutofillCreditCardEnabled = false;
+      DisableAppUpdate = true;
+      DisableFeedbackCommands = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DontCheckDefaultBrowser = true;
+      NoDefaultBookmarks = true;
+      OfferToSaveLogins = false;
+      EnableTrackingProtection = {
+        Value = true;
+        Locked = true;
+        Cryptomining = true;
+        Fingerprinting = true;
+      };
+    };
   };
 }
