0737ff5507411bdffcc2abda417592a9676fcbec
Thierry Delafontaine <delafthi@pm.me>
2025-12-18 09:28:52 +0100

    fix(dev): remove unsupported regex lookarounds in atuin filters

--- home/shared/dev/shells/atuin.nix
+++ home/shared/dev/shells/atuin.nix
@@ -8,15 +8,15 @@
       filter_mode_shell_up_key_binding = "directory";
       history_filter = [
         # security
-        "--api-key(?:[= ](?!\\$\\())"
-        "--password(?:[= ](?!\\$\\())"
-        "--secret(?:[= ](?!\\$\\())"
-        "--token(?:[= ](?!\\$\\())"
+        "--api-key[= ]"
+        "--password[= ]"
+        "--secret[= ]"
+        "--token[= ]"
         ".*-u\\s+\\w+:"
-        "_API_KEY=(?!\\$\\()"
-        "_PASSWORD=(?!\\$\\()"
-        "_SECRET=(?!\\$\\()"
-        "_TOKEN=(?!\\$\\()"
+        "_API_KEY="
+        "_PASSWORD="
+        "_SECRET="
+        "_TOKEN="
         "^curl.*(-u|--user)"
         # misc
         "^exit$"
