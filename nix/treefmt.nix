cbe72bd6932cb3d19f9220225ed909b77d458146
Thierry Delafontaine <delafthi@pm.me>
2026-01-29 14:42:20 +0100

    chore: use rumdl for markdown formatting

--- nix/treefmt.nix
+++ nix/treefmt.nix
@@ -10,13 +10,8 @@
     fish_indent.enable = true;
     just.enable = true;
     keep-sorted.enable = true;
-    mdformat = {
-      enable = true;
-      plugins = ps: [
-        ps.mdformat-frontmatter
-      ];
-    };
     nixfmt.enable = true;
+    rumdl-check.enable = true;
     shellcheck.enable = true;
     shfmt.enable = true;
     statix.enable = true;
