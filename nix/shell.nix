4ee05ac6d18c2e84fc02accc3d31ee1c311df78c
Thierry Delafontaine <delafthi@pm.me>
2026-02-09 20:35:18 +0100

    fix(direnv): watch `./nix/shell.nix`

--- nix/shell.nix
+++ nix/shell.nix
@@ -1,13 +1,18 @@
-{ config, pkgs }:
 {
-  default = pkgs.mkShell {
-    name = "dotfiles";
-    inputsFrom = [ config.treefmt.build.devShell ];
-    packages = with pkgs; [
-      age
-      age-plugin-yubikey
-      nixd
-      sops
-    ];
-  };
+  mkShell,
+  age,
+  age-plugin-yubikey,
+  nixd,
+  sops,
+  config,
+}:
+mkShell {
+  name = "dotfiles";
+  inputsFrom = [ config.treefmt.build.devShell ];
+  packages = [
+    age
+    age-plugin-yubikey
+    nixd
+    sops
+  ];
 }
