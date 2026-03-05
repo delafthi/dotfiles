eaf709060788d5ec6eb3b0400c8c16e5c03454bb
Thierry Delafontaine <delafthi@pm.me>
2026-03-05 20:15:24 +0100

    feat(system/nix) add `nix-community.cachix.org` as substituter

--- system/shared/nix.nix
+++ system/shared/nix.nix
@@ -19,6 +19,12 @@
         "nix-command"
         "pipe-operators"
       ];
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
       trusted-users = if pkgs.stdenv.hostPlatform.isDarwin then [ "@admin" ] else [ "@wheel" ];
     };
   };
