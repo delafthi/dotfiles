107b65fcc700930f96ddd743846a273500ec4983
Thierry Delafontaine <delafthi@pm.me>
2026-03-13 18:00:43 +0100

    fix(nix): add substituters to `nixConfig` in `flake.nix`

--- flake.nix
+++ flake.nix
@@ -2,8 +2,14 @@
   description = "delafthi's dotfiles";
 
   nixConfig = {
-    extra-trusted-substituters = [ "https://cache.flox.dev" ];
-    extra-trusted-public-keys = [ "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs=" ];
+    extra-substituters = [
+      "https://cache.flox.dev"
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
   inputs = {
