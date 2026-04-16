e1ed9e4d356e67a9f25a3210bb7c6238670b28ff
Thierry Delafontaine <deaa@zhaw.ch>
2026-04-16 13:40:11 +0200

    feat(packages): move my wallpaper into a dedicated package

--- home/darwin/desktop/desktoppr.nix
+++ home/darwin/desktop/desktoppr.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  pkgs,
   osConfig,
   ...
 }:
@@ -7,8 +8,7 @@ lib.mkIf osConfig.system.gui.enable {
   programs.desktoppr = {
     enable = true;
     settings = {
-      picture = "https://unsplash.com/photos/1527pjeb6jg/download?force=true";
-      sha256 = "3994d494d4c5ad50fb47e47d9f7b13197676b06a2baae9a21a6ea9a3cc517467";
+      picture = "${pkgs.wallpaper}/share/wallpapers/wallpaper.jpg";
     };
   };
 }
