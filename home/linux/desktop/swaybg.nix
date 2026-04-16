b487d6f46bf2e0cd9b2567bbd5b8e8151e92d120
Thierry Delafontaine <delafthi@pm.me>
2026-04-16 23:35:11 +0200

    feat(home/linux/desktop/swaybg): improve swaybg service

--- home/linux/desktop/swaybg.nix
+++ home/linux/desktop/swaybg.nix
@@ -1,4 +1,5 @@
 {
+  config,
   lib,
   osConfig,
   pkgs,
@@ -8,10 +9,13 @@ lib.mkIf osConfig.system.gui.enable {
   systemd.user.services.swaybg = {
     Unit = {
       Description = "Wallpaper daemon";
-      After = [ "graphical-session.target" ];
-      PartOf = [ "graphical-session.target" ];
+      After = [ config.wayland.systemd.target ];
+      PartOf = [ config.wayland.systemd.target ];
+      ConditionEnvironment = "WAYLAND_DISPLAY";
+      StartLimitBurst = 5;
+      StartLimitIntervalSec = 10;
     };
-    Install.WantedBy = [ "graphical-session.target" ];
+    Install.WantedBy = [ config.wayland.systemd.target ];
     Service = {
       ExecStart = "${pkgs.swaybg}/bin/swaybg -i ${pkgs.wallpaper}/share/wallpapers/wallpaper.jpg -m fill";
       Restart = "on-failure";
