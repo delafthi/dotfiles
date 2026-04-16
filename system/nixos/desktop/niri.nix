a14ee70b67bb92eec0380076dede849ad1b458dd
Thierry Delafontaine <delafthi@pm.me>
2026-04-16 23:14:32 +0200

    fix(system/nixos/desktop/niri): remove duplicate configuration
    Remove configuration which is already set by enabling `programs.niri.enable = true`.

--- system/nixos/desktop/niri.nix
+++ system/nixos/desktop/niri.nix
@@ -6,18 +6,6 @@
 }:
 lib.mkIf config.system.gui.enable {
   programs.niri.enable = true;
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gnome
-      xdg-desktop-portal-gtk
-    ];
-    config = {
-      common.default = "*";
-      niri."org.freedesktop.impl.portal.FileChooser" = [ "gtk" ];
-    };
-  };
-  services.gnome.gnome-keyring.enable = true;
   security.pam.services.swaylock = { };
   environment.systemPackages = with pkgs; [
     brightnessctl
